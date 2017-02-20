6a472514e64ce208a1e84d2adf7e3a736d7d9129
Setsuodu <339131001@qq.com>
2017-02-20 11:50:07 +0800

    upload server side

--- nodeGameServer/index.js
+++ nodeGameServer/index.js
@@ -18,8 +18,8 @@ io.on('connection', function(socket){
 	var currentPlayer = {};
 	currentPlayer.name = 'unknown';
 
-	socket.on('other player connected',function(){
-		console.log(currentPlayer.name + 'recv: other player connect');
+	socket.on('player connect',function(){
+		console.log(currentPlayer.name+' recv: player connect');
 		for(var i = 0; i < clients.length; i++){
 			var playerConnected = {
 				name:clients[i].name,
@@ -29,7 +29,7 @@ io.on('connection', function(socket){
 			};
 			// in your current game, we need to tell you about the other players.
 			socket.emit('other player connected', playerConnected);
-			console.log(currentPlayer.name + 'emit: other player connected: ' + JSON.stringify(playerConnected));
+			console.log(currentPlayer.name+' emit: other player connected: '+JSON.stringify(playerConnected));
 		}
 	});
 	/*
@@ -39,7 +39,7 @@ io.on('connection', function(socket){
 	});
 	*/
 	socket.on('play',function(data){
-		console.log(currentPlayer.name + ' recv:play: ' + JSON.stringify(data));
+		console.log(currentPlayer.name+' recv: play: '+JSON.stringify(data));
 		// if this is the first person to join the game init the enemies
 		if(clients.length === 0){
 			numberOfEnemies = data.enemySpawnPoints.length;
@@ -69,7 +69,7 @@ io.on('connection', function(socket){
 		//we always will send the enemies when the player joins
 		console.log(currentPlayer.name+' emit: enemies: '+JSON.stringify(enemiesResponse));
 		socket.emit('enemies', enemiesResponse);
-		var randomSpawnPoint = playerSpawnPoints[Math.floor(Math.random()*playerSpawnPoints.length)];
+		var randomSpawnPoint = playerSpawnPoints[Math.floor(Math.random() * playerSpawnPoints.length)];
 		currentPlayer = {
 			name: data.name,
 			position: randomSpawnPoint.position,
@@ -79,47 +79,49 @@ io.on('connection', function(socket){
 		clients.push(currentPlayer);
 		// in your current game, tell you that you have joined
 		console.log(currentPlayer.name+' emit: play: '+JSON.stringify(currentPlayer));
-		socket.emit('play',currentPlayer);
+		socket.emit('play', currentPlayer);
 		// in your current game, we need to tell the other players about you.
 		socket.broadcast.emit('other player connected', currentPlayer);
 	});
 	
 	socket.on('player move',function(data){
-		console.log('recv: move: ' + JSON.stringify(data));
+		console.log('recv: move: '+JSON.stringify(data));
 		currentPlayer.position = data.position;
 		socket.broadcast.emit('player move', currentPlayer);
 	});
 
 	socket.on('player turn', function(data){
-		console.log('recv: turn: ' + JSON.stringify(data));
-		currentPlayer.rotation = data.rotationturn;
+		console.log('recv: turn: '+JSON.stringify(data));
+		currentPlayer.rotation = data.rotation;
 		socket.broadcast.emit('player turn', currentPlayer);
 	});
 
 	socket.on('player shoot',function(data){
-		console.log(currentPlayer.name + 'recv: shoot');
+		console.log(currentPlayer.name+' recv: shoot');
 		var data = {
 			name: currentPlayer.name
 		};
-		console.log(currentPlayer.name+' bcst: shoot: '+ JSON.stringify(data));
-		socket.emit('player shoot',data);
-		socket.broadcast.emit('player shoot',data);
+		console.log(currentPlayer.name+' bcst: shoot: '+JSON.stringify(data));
+		socket.emit('player shoot', data);
+		socket.broadcast.emit('player shoot', data);
 	});
 
 	socket.on('health',function(data){
-		console.log(currentPlayer.name+ 'recv: health: ' + JSON.stringify(data));
+		console.log(currentPlayer.name+' recv: health: '+JSON.stringify(data));
 		// only change the health once, we can do this by checking the originating player
 		if(data.from === currentPlayer.name){
 			var indexDamaged = 0;
-			if(!data.isEnemy){
-				clients = clients.map(function(client, index){
-					indexDamaged = index;
-					client.health -= data.healthChange;
+			if(!data.isEnemy) {
+				clients = clients.map(function(client, index) {
+					if(client.name === data.name){
+						indexDamaged = index;
+						client.health -= data.healthChange;
+					}
+					return client;
 				});
-				return client;
-			}else{
-				enemies = enemies.map(function(enemy,index){
-					if(enemy.name === data.name){
+			} else {
+				enemies = enemies.map(function(enemy, index) {
+					if(enemy.name === data.name) {
 						indexDamaged = index;
 						enemy.health -= data.healthChange;
 					}
@@ -130,17 +132,17 @@ io.on('connection', function(socket){
 			var response = {
 				name: (!data.isEnemy) ? clients[indexDamaged].name : enemies[indexDamaged].name,
 				health: (!data.isEnemy) ? clients[indexDamaged].health : enemies[indexDamaged].health,
-			}
-			console.log(currentPlayer.name+' bcst: health: ' + JSON.stringify(response));
+			};
+			console.log(currentPlayer.name+' bcst: health: '+JSON.stringify(response));
 			socket.emit('health',response);
 			socket.broadcast.emit('health',response);
 		}
 	});
 
 	socket.on('disconnect', function(){
-		console.log(currentPlayer.name+ ' recv: disconnect ' + currentPlayer.name);
+		console.log(currentPlayer.name+' recv: disconnect: '+currentPlayer.name);
 		socket.broadcast.emit('other player disconnected', currentPlayer);
-		console.log(currentPlayer.name+ ' bcst: other player disconnected '+JSON.stringify(currentPlayer));
+		console.log(currentPlayer.name+' bcst: other player disconnected '+JSON.stringify(currentPlayer));
 		for(var i=0; i<clients.length; i++){
 			if(clients[i].name === currentPlayer.name){
 				clients.splice(i,1);
